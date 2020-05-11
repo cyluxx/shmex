e14bc2dbd1df4fc13818e476e622b06eee868d3b
Andreas Plewnia <acplewnia@gmail.com>
2020-05-12 01:34:03 +0200

    Add shmexl to codemirror

---
+++ src/shmexl.js
@@ -0,0 +1,13 @@
+import CodeMirror from "codemirror";
+import 'codemirror/addon/mode/simple';
+
+CodeMirror.defineSimpleMode("shmexl", {
+  start: [{ regex: / ?/, token: "atom", next: "duration" }],
+  duration: [
+    { regex: /[1-9][0-9]*\/(1(?!6)|2|4|8|16|32)/, token: "atom", next: "pitch" }
+  ],
+  pitch: [
+    { regex: /[a-g][#b]?[0-8]/, token: "keyword" },
+    { regex: /,/, token: "operator", next: "duration" }
+  ]
+});
