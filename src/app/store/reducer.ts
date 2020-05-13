3c5b8440e2cd793da758ab457b9dc39615dcce10
Andreas Plewnia <acplewnia@gmail.com>
2020-05-13 17:14:34 +0200

    Implement parse shmexl reducer

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -1,5 +1,5 @@
 import {Action, createReducer, on} from "@ngrx/store";
-import {AppState, initialAppState} from "./state";
+import {AppState, initialAppState, RhythmElement} from "./state";
 import {parseShmexlText} from "./actions";
 import 'codemirror/addon/runmode/runmode'
 import * as CodeMirror from "codemirror";
@@ -8,10 +8,39 @@ const _reducer = createReducer(
   initialAppState,
 
   on(parseShmexlText, (state, {shmexlText}) => {
-    CodeMirror.runMode(this.codeMirrorContent, 'shmexl', (token, style) => {
-
+    const rhythmElements: RhythmElement[] = [];
+    const next: RhythmElement = {duration: null, tones: []};
+    CodeMirror.runMode(shmexlText, 'shmexl', (token, style) => {
+      switch (style) {
+        case 'atom':
+          const splitDurationToken = token.split('/');
+          next.duration = {
+            numerator: +splitDurationToken[0],
+            denominator: +splitDurationToken[1] as 1 | 2 | 4 | 8 | 16 | 32
+          }
+          break;
+        case 'keyword':
+          const splitToneToken = token.split('');
+          if (splitToneToken.length === 2) {
+            next.tones.push({
+              tone: splitToneToken[0] as 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g',
+              octave: +splitToneToken[1] as 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+            });
+          } else {
+            next.tones.push({
+              tone: splitToneToken[0] as 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g',
+              accidental: splitToneToken[1] as '#' | 'b',
+              octave: +splitToneToken[2] as 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+            })
+          }
+          break;
+        case 'operator':
+          rhythmElements.push(next);
+          next.tones = [];
+          break;
+      }
     });
-    return state;
+    return {...state, track: {...state.track, rhythmElements}};
   }),
 );
 
--- src/shmexl.js
+++ src/shmexl.js
@@ -2,7 +2,7 @@ import CodeMirror from "codemirror";
 import 'codemirror/addon/mode/simple';
 
 CodeMirror.defineSimpleMode("shmexl", {
-  start: [{ regex: / ?/, token: "atom", next: "duration" }],
+  start: [{ regex: / ?/, next: "duration" }],
   duration: [
     { regex: /[1-9][0-9]*\/(1(?!6)|2|4|8|16|32)/, token: "atom", next: "tone" }
   ],
