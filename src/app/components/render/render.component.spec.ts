a3ce4f98be2d6e6b3b6cc8fba71da481f2c43c78
Andreas Plewnia <acplewnia@gmail.com>
2020-07-07 16:09:55 +0200

    Fix tests

--- src/app/components/render/render.component.spec.ts
+++ src/app/components/render/render.component.spec.ts
@@ -1,21 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RenderComponent } from './render.component';
+import {RenderComponent} from './render.component';
+import {MockStore, provideMockStore} from "@ngrx/store/testing";
+import {initialAppState} from "../../store/state";
 
 describe('RenderComponent', () => {
   let component: RenderComponent;
   let fixture: ComponentFixture<RenderComponent>;
+  let store: MockStore;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RenderComponent ]
+      declarations: [RenderComponent],
+      providers: [provideMockStore({initialState: {app: initialAppState}})],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RenderComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
