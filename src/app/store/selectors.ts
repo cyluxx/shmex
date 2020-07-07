6c84898d9bb5ee65d981880599c512baa2e1b888
Andreas Plewnia <acplewnia@gmail.com>
2020-07-07 16:02:37 +0200

    Remove preview component

--- src/app/store/selectors.ts
+++ src/app/store/selectors.ts
@@ -1,6 +1,5 @@
 import {createSelector} from "@ngrx/store";
 import {AppState, Track} from "./state";
-import {getEndingRests, isRest, toVexRest, toVexTones} from "./utils";
 import {buildPart, buildPartList, finalize} from "../utils/music-xml-builder";
 
 const selectAppState = state => state.app;
@@ -8,22 +7,6 @@ const selectAppState = state => state.app;
 export const selectCurrentTrack = createSelector(
   selectAppState,
   (state: AppState) => state.track
-);
-
-export const selectCurrentTrackNotes = createSelector(
-  selectCurrentTrack,
-  (track: Track) => {
-    const staveNotes = track.rhythmElements.map(rhythmElement => {
-      if (isRest(rhythmElement)) {
-        return toVexRest(rhythmElement);
-      }
-      return toVexTones(rhythmElement);
-    });
-    const endingRests = getEndingRests(track).map(rhythmElement => {
-      return toVexRest(rhythmElement);
-    });
-    return staveNotes.concat(endingRests);
-  }
 );
 
 export const selectMusicXml = createSelector(
