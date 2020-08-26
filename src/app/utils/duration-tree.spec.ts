1101d0de13694849c2c05c8143167fb43d244d56
Andreas Plewnia <acplewnia@gmail.com>
2020-08-26 22:27:04 +0200

    Add position to duration tree

--- src/app/utils/duration-tree.spec.ts
+++ src/app/utils/duration-tree.spec.ts
@@ -1,14 +1,258 @@
-import {constructDurationTree, DurationVertex} from './duration-tree';
+import {constructDurationTree} from './duration-tree';
 
 describe('constructDurationTree', () => {
   it('constructs correct 4/4 tree', () => {
-    const thirtySecond: DurationVertex = {value: 32};
-    const sixteenth: DurationVertex = {value: 16, leftChild: thirtySecond, rightChild: thirtySecond};
-    const eighth: DurationVertex = {value: 8, leftChild: sixteenth, rightChild: sixteenth};
-    const quarter: DurationVertex = {value: 4, leftChild: eighth, rightChild: eighth};
-    const half: DurationVertex = {value: 2, leftChild: quarter, rightChild: quarter};
-    const whole: DurationVertex = {value: 1, leftChild: half, rightChild: half};
-
-    expect(constructDurationTree()).toEqual(whole);
+    expect(constructDurationTree()).toEqual({
+      noteValue: 1,
+      position: 0,
+      leftChild: {
+        noteValue: 2,
+        position: 0,
+        leftChild: {
+          noteValue: 4,
+          position: 0,
+          leftChild: {
+            noteValue: 8,
+            position: 0,
+            leftChild: {
+              noteValue: 16,
+              position: 0,
+              leftChild: {
+                noteValue: 32,
+                position: 0,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 1,
+              }
+            },
+            rightChild: {
+              noteValue: 16,
+              position: 2,
+              leftChild: {
+                noteValue: 32,
+                position: 2,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 3,
+              }
+            }
+          },
+          rightChild: {
+            noteValue: 8,
+            position: 4,
+            leftChild: {
+              noteValue: 16,
+              position: 4,
+              leftChild: {
+                noteValue: 32,
+                position: 4,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 5,
+              }
+            },
+            rightChild: {
+              noteValue: 16,
+              position: 6,
+              leftChild: {
+                noteValue: 32,
+                position: 6,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 7,
+              }
+            }
+          }
+        },
+        rightChild: {
+          noteValue: 4,
+          position: 8,
+          leftChild: {
+            noteValue: 8,
+            position: 8,
+            leftChild: {
+              noteValue: 16,
+              position: 8,
+              leftChild: {
+                noteValue: 32,
+                position: 8,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 9,
+              }
+            },
+            rightChild: {
+              noteValue: 16,
+              position: 10,
+              leftChild: {
+                noteValue: 32,
+                position: 10,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 11,
+              }
+            }
+          },
+          rightChild: {
+            noteValue: 8,
+            position: 12,
+            leftChild: {
+              noteValue: 16,
+              position: 12,
+              leftChild: {
+                noteValue: 32,
+                position: 12,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 13,
+              }
+            },
+            rightChild: {
+              noteValue: 16,
+              position: 14,
+              leftChild: {
+                noteValue: 32,
+                position: 14,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 15,
+              }
+            }
+          }
+        }
+      },
+      rightChild: {
+        noteValue: 2,
+        position: 16,
+        leftChild: {
+          noteValue: 4,
+          position: 16,
+          leftChild: {
+            noteValue: 8,
+            position: 16,
+            leftChild: {
+              noteValue: 16,
+              position: 16,
+              leftChild: {
+                noteValue: 32,
+                position: 16,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 17,
+              }
+            },
+            rightChild: {
+              noteValue: 16,
+              position: 18,
+              leftChild: {
+                noteValue: 32,
+                position: 18,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 19,
+              }
+            }
+          },
+          rightChild: {
+            noteValue: 8,
+            position: 20,
+            leftChild: {
+              noteValue: 16,
+              position: 20,
+              leftChild: {
+                noteValue: 32,
+                position: 20,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 21,
+              }
+            },
+            rightChild: {
+              noteValue: 16,
+              position: 22,
+              leftChild: {
+                noteValue: 32,
+                position: 22,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 23,
+              }
+            }
+          }
+        },
+        rightChild: {
+          noteValue: 4,
+          position: 24,
+          leftChild: {
+            noteValue: 8,
+            position: 24,
+            leftChild: {
+              noteValue: 16,
+              position: 24,
+              leftChild: {
+                noteValue: 32,
+                position: 24,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 25,
+              }
+            },
+            rightChild: {
+              noteValue: 16,
+              position: 26,
+              leftChild: {
+                noteValue: 32,
+                position: 26,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 27,
+              }
+            }
+          },
+          rightChild: {
+            noteValue: 8,
+            position: 28,
+            leftChild: {
+              noteValue: 16,
+              position: 28,
+              leftChild: {
+                noteValue: 32,
+                position: 28,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 29,
+              }
+            },
+            rightChild: {
+              noteValue: 16,
+              position: 30,
+              leftChild: {
+                noteValue: 32,
+                position: 30,
+              },
+              rightChild: {
+                noteValue: 32,
+                position: 31,
+              }
+            }
+          }
+        }
+      }
+    });
   });
 });
--- src/app/utils/duration-tree.ts
+++ src/app/utils/duration-tree.ts
@@ -3,7 +3,8 @@
  */
 
 export interface DurationVertex {
-  value: 1 | 2 | 4 | 8 | 16 | 32;
+  noteValue: 1 | 2 | 4 | 8 | 16 | 32;
+  position: number;
   leftChild?: DurationVertex;
   rightChild?: DurationVertex;
 }
@@ -13,25 +14,39 @@ export interface DurationVertex {
  * see: https://music.stackexchange.com/a/23049/71319
  */
 export function constructDurationTree(): DurationVertex {
-  return constructDurationTreeRecursive(1);
+  return constructDurationTreeRecursive(1, 0);
 }
 
-function constructDurationTreeRecursive(value: 1 | 2 | 4 | 8 | 16 | 32): DurationVertex {
-  if (value === 32) {
+function constructDurationTreeRecursive(noteValue: 1 | 2 | 4 | 8 | 16 | 32, position: number): DurationVertex {
+  if (noteValue === 32) {
     return {
-      value
+      noteValue,
+      position
     };
   }
+  const doubledNoteValue = doubleNoteValue(noteValue);
   return {
-    value,
-    leftChild: constructDurationTreeRecursive(doubleDurationValue(value)),
-    rightChild: constructDurationTreeRecursive(doubleDurationValue(value)),
+    noteValue,
+    position,
+    leftChild: constructDurationTreeRecursive(doubledNoteValue, position),
+    rightChild: constructDurationTreeRecursive(doubledNoteValue, position + positionValueOf(doubledNoteValue)),
   };
 }
 
-function doubleDurationValue(value: 1 | 2 | 4 | 8 | 16 | 32): 1 | 2 | 4 | 8 | 16 | 32 {
-  if (value === 32) {
+function positionValueOf(noteValue: 1 | 2 | 4 | 8 | 16 | 32): number {
+  switch (noteValue) {
+    case 1: return 32;
+    case 2: return 16;
+    case 4: return 8;
+    case 8: return 4;
+    case 16: return 2;
+    case 32: return 1;
+  }
+}
+
+function doubleNoteValue(noteValue: 1 | 2 | 4 | 8 | 16 | 32): 1 | 2 | 4 | 8 | 16 | 32 {
+  if (noteValue === 32) {
     return 32;
   }
-  return value * 2 as 1 | 2 | 4 | 8 | 16 | 32;
+  return noteValue * 2 as 1 | 2 | 4 | 8 | 16 | 32;
 }
