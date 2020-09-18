3933527605365b8c965943d89d210da3f73ed2fa
Andreas Plewnia <acplewnia@gmail.com>
2020-09-18 17:22:53 +0200

    Add app component test (wip)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { initialAppState } from './store/model';
+import { AppState, initialAppState } from './store/model';
+import { ToolbarState } from './store/enum';
 
 describe('AppComponent', () => {
-  let store: MockStore;
+  let store: MockStore<AppState>;
+  let component: AppComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       providers: [provideMockStore({ initialState: { app: initialAppState } })],
     }).compileComponents();
+    const fixture = TestBed.createComponent(AppComponent);
+    store = TestBed.inject(MockStore);
+    component = fixture.componentInstance;
+    component.ngOnInit();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
+
+  it('should show the edit component, when the toolbar state is EDIT_SHEETS', () => {
     store = TestBed.inject(MockStore);
-    expect(app).toBeTruthy();
+
+    store.setState({ ...initialAppState, toolbar: { ...initialAppState.toolbar, state: ToolbarState.EDIT_SHEETS } });
+
+    component.toolbarState$.subscribe((next) => expect(next).toBe(ToolbarState.EDIT_SHEETS));
   });
 });
