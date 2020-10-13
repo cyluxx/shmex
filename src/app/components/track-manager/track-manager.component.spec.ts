c491346a77f6f376af596f6b62f7dbe28b276c43
Andreas Plewnia <acplewnia@gmail.com>
2020-10-13 19:37:00 +0200

    Select tracks for track manager

--- src/app/components/track-manager/track-manager.component.spec.ts
+++ src/app/components/track-manager/track-manager.component.spec.ts
@@ -1,19 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TrackManagerComponent } from './track-manager.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { AppState, initialAppState } from '../../store/model';
+import { ExportService } from '../../service/export.service';
 
 describe('TrackManagerComponent', () => {
   let component: TrackManagerComponent;
   let fixture: ComponentFixture<TrackManagerComponent>;
+  let store: MockStore<{ app: AppState }>;
+  let mockExportService: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TrackManagerComponent],
-    }).compileComponents();
-  });
+  beforeEach(
+    waitForAsync(() => {
+      mockExportService = jasmine.createSpyObj(['exportMusicXml']);
+      TestBed.configureTestingModule({
+        declarations: [TrackManagerComponent],
+        providers: [
+          provideMockStore({ initialState: { app: initialAppState } }),
+          { provide: ExportService, useValue: mockExportService },
+        ],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TrackManagerComponent);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/components/track-manager/track-manager.component.ts
+++ src/app/components/track-manager/track-manager.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Track } from '../../store/model';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectTracks } from '../../store/selectors';
 
 @Component({
   selector: 'app-track-manager',
@@ -8,17 +11,15 @@ import { Track } from '../../store/model';
   styleUrls: ['./track-manager.component.css'],
 })
 export class TrackManagerComponent implements OnInit {
-  tracks: Track[] = [
-    {
-      name: 'test',
-      measures: [],
-      id: '',
-    },
-  ];
+  tracks$: Observable<Track[]>;
+  tracks: Track[];
 
-  constructor() {}
+  constructor(private store: Store) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.tracks$ = this.store.select(selectTracks);
+    this.tracks$.subscribe((tracks) => (this.tracks = tracks));
+  }
 
   onDrop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.tracks, event.previousIndex, event.currentIndex);
