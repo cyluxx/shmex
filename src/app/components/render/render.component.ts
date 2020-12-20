abeef0c7cb33cd53bcc86ab26b236545990429ee
Andreas Plewnia <acplewnia@gmail.com>
2020-12-20 15:04:08 +0100

    Add simple audio player

--- src/app/components/render/render.component.ts
+++ src/app/components/render/render.component.ts
@@ -1,10 +1,12 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay';
 import { Store } from '@ngrx/store';
-import { selectMusicXml } from '../../store/selectors';
+import { selectAudioPlayerState, selectMusicXml } from '../../store/selectors';
 import { Observable, Subscription } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import format from 'xml-formatter';
+import AudioPlayer from 'osmd-audio-player';
+import { AudioPlayerState } from '../../store/enum';
 
 @Component({
   selector: 'app-render',
@@ -17,14 +19,20 @@ export class RenderComponent implements AfterViewInit, OnInit, OnDestroy {
   musicXml$: Observable<string>;
   musicXmlSubscription: Subscription;
 
+  audioPlayerState$: Observable<AudioPlayerState>;
+  audioPlayerSubscription: Subscription;
+
   constructor(private store: Store) {}
 
   ngOnInit(): void {
     this.musicXml$ = this.store.select(selectMusicXml);
+    this.audioPlayerState$ = this.store.select(selectAudioPlayerState);
   }
 
   ngAfterViewInit(): void {
     const osmd = new OpenSheetMusicDisplay(this.container.nativeElement);
+    const audioPlayer = new AudioPlayer();
+
     this.musicXmlSubscription = this.musicXml$
       .pipe(
         switchMap((next) => {
@@ -32,12 +40,24 @@ export class RenderComponent implements AfterViewInit, OnInit, OnDestroy {
           return osmd.load(next);
         })
       )
-      .subscribe(() => {
+      .subscribe(async () => {
         osmd.render();
+        await audioPlayer.loadScore(osmd);
       });
+
+    this.audioPlayerSubscription = this.audioPlayerState$.subscribe(async (next) => {
+      if (next === AudioPlayerState.PLAY) {
+        await audioPlayer.play();
+      } else if (next === AudioPlayerState.PAUSE) {
+        await audioPlayer.pause();
+      } else {
+        await audioPlayer.stop();
+      }
+    });
   }
 
   ngOnDestroy() {
     this.musicXmlSubscription.unsubscribe();
+    this.audioPlayerSubscription.unsubscribe();
   }
 }
